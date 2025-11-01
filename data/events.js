d4f74a9b4fa623633c46d2face249b7c2813ca16
KeisukeSawa <sawa.window@outlook.jp>
2025-11-01 17:47:49 +0900

    [change] 11月イベント日程に修正

--- data/events.js
+++ data/events.js
@@ -1,12 +1,12 @@
 const events = [
   {
-    id: "2",
-    title: "第32回CoderDojoたまち",
-    date: "2025年9月15日（月）",
+    id: "1",
+    title: "第33回CoderDojoたまち",
+    date: "2025年11月22日（土）",
     time: "09:30-11:30",
     description: "東京都港区 田町駅周辺でみんなでプログラミングを楽しもう",
     venue: "三田いきいきプラザ（東京都港区芝4-1-17）",
-    url: "https://coderdojo-tamachi.connpass.com/event/368423/",
+    url: "https://coderdojo-tamachi.connpass.com/event/374588/",
   },
 ]
 
