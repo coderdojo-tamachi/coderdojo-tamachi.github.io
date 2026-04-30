d030ab706b2c161949faf95f292bb2001f396605
KeisukeSawa <sawa.window@outlook.jp>
2026-04-30 22:11:38 +0900

    [change] 5月回の情報更新

--- data/events.js
+++ data/events.js
@@ -1,12 +1,12 @@
 const events = [
   {
     id: "1",
-    title: "第38回CoderDojoたまち",
-    date: "2026年4月12日（日）",
+    title: "第39回CoderDojoたまち",
+    date: "2026年5月24日（日）",
     time: "09:30-11:30",
     description: "東京都港区 田町駅周辺でみんなでプログラミングを楽しもう",
     venue: "三田いきいきプラザ（東京都港区芝4-1-17）",
-    url: "https://coderdojo-tamachi.connpass.com/event/388366/",
+    url: "https://coderdojo-tamachi.connpass.com/event/390890/",
   },
 ]
 
